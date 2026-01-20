2f412b54d90ec963f32eeab530b5356d70cabdcb
mfelix <michael@mfelix.org>
2026-01-20 13:48:35 -0500

    Add changelog entries for recent features

--- js/changelog.js
+++ js/changelog.js
@@ -1,6 +1,22 @@
 // Auto-generated changelog - do not edit manually
 const CHANGELOG = [
   {
+    "message": "Routine UFO maintenance",
+    "timestamp": 1768934864000
+  },
+  {
+    "message": "Redesign title screen with tabbed navigation",
+    "timestamp": 1768934864000
+  },
+  {
+    "message": "Add player feedback system with ratings and suggestions",
+    "timestamp": 1768934864000
+  },
+  {
+    "message": "Expand leaderboard to top 100 players",
+    "timestamp": 1768934864000
+  },
+  {
     "message": "Improve UFO Shopping Mall",
     "timestamp": 1768770781000
   },
